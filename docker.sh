f4dbd391227f67600c84a0f432e7ea0f215bad0e
thanhtamdc <duongchauthanhtam@yahoo.com>
2025-10-08 11:21:44 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,7 +12,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
