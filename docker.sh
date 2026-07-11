cf66266ed7fa608b9ae57928355eb68ecef64f70
thanhtamdc <duongchauthanhtam@yahoo.com>
2026-07-11 09:34:54 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -27,3 +27,14 @@ echo "alias dc='docker compose'" >> ~/.bash_profile
 echo "alias upb='source ~/.bashrc'" >> ~/.bash_profile
 echo "upb" >> ~/.bash_profile
 . ~/.bash_profile
+
+
+#Ubuntu 26.04
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
